253ad8993c44235f7d6b4133a25013941d21e702
Vigneshwaran S <vigneshwarans013@gmail.com>
2020-12-12 17:48:18 +0530

    added value and reference types

---
+++ udmey/Beginner/4NonPrimitiveTypes/RefTypesAndValues.md
@@ -0,0 +1,39 @@
+## RefTypes and values
+In csharp we have two main types from which we create new type
+
+1. Structures
+2. Classes
+
+## Structures
+
+- primtive types
+- Custom strucures
+- 
+
+## Classes
+
+- Arrays
+- Strings
+- Custom classes
+
+## Value Tpes(Structures)
+
+- when we create varible in value type a part of memory called `stack` is allocated
+for that varible
+- Memory allocation done automatically
+- when this variable get out of scope it will immediately
+get removed from stack by runtime /CLR
+ 
+
+##  Reference Types (Classes)
+
+- Programmer need to allocate the memory with the new operator
+- Memory alloccation happen in different area of memory called `heap`
+- This memory is more sustainable
+- so the object created in heap memory will continue in memory for little
+while even if the object get out of scope
+- it will get removed by the process called `Garbage collector` while runtime or by CLR
+
+
+## Demo
+
---
+++ udmey/Beginner/4NonPrimitiveTypes/codefiles/values_ref_demo1/Program.cs
@@ -0,0 +1,34 @@
+﻿using CSharpFundamentals.Math;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFundamentals
+{
+    
+   
+    class Program
+    {   
+        
+        static void Main(string[] args)
+        {
+            // value type will create a copy of var with new memory location in stack
+            var a = 5;
+            var b = a;
+            b++;
+            Console.WriteLine(string.Format("a :{0} b : {1}",a,b));// a=5,b=6
+
+            /*reference type will copy of the refrence of the variable  which is present in heap to the stack
+            so if we assign to other var then it will create another reference in stack which will refernce to
+            the same heap memory location*/
+
+            var numbers = new int[3] { 1, 2, 3 };
+            var numbers2 = numbers;
+            numbers2[0] = 200;
+            Console.WriteLine(string.Format("numbers[0] is : {0} and  numbers2[0] :{1}", numbers[0], numbers2[0]));
+
+        }
+    }
+}
