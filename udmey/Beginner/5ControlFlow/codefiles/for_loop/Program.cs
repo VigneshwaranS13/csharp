fbcf75517d2dd12922d0b3b77799e40070f1cbf7
Vigneshwaran S <vigneshwarans013@gmail.com>
2020-12-13 14:30:24 +0530

    added for and foreach demo code

---
+++ udmey/Beginner/5ControlFlow/codefiles/for_loop/Program.cs
@@ -0,0 +1,46 @@
+﻿using CSharpFundamentals.Math;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFundamentals 
+{ 
+    
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+           
+            // To print odd or even number within 10
+            for (var i = 1; i <= 10; i++)
+            {
+                if (i % 2  == 0)
+                {
+                    Console.WriteLine(string.Format("Even number : {0}",i));
+                }
+                else
+                {
+                    Console.WriteLine(string.Format("Odd number : {0}",i));
+                }
+            }
+
+            // To reverse the order
+            for (var i = 10; i >=1; i--)
+            {
+                if( i % 2 ==0 )
+                {
+                    Console.WriteLine(string.Format("even number : {0}", i));
+                }
+
+                else
+                {
+                    Console.WriteLine(string.Format("odd number : {0}", i));
+                }
+            }
+            
+        }
+    }
+}
---
+++ udmey/Beginner/5ControlFlow/codefiles/foreach_loop/Program.cs
@@ -0,0 +1,38 @@
+﻿using CSharpFundamentals.Math;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFundamentals 
+{ 
+    
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+           var name = "MS Dhoni";
+           // with for loop
+           for (var i = 0; i < name.Length; i++ )
+            {
+                Console.WriteLine(name[i]);
+            }
+            // with foreach
+
+           foreach(var letter in name)
+            {
+                Console.WriteLine(letter);
+            }
+
+            // foreach for array
+
+            var numbers = new int[5] { 1, 2, 3, 4, 5 };
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
