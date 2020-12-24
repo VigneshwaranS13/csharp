9bcefd3bd6c65cfbe13debed8e68075a002dc325
Vigneshwaran S <vigneshwarans013@gmail.com>
2020-12-24 22:01:36 +0530

    Added path demo file

---
+++ udmey/Beginner/9WorkingWithFiles/CodeFiles/Path.cs
@@ -0,0 +1,42 @@
+﻿using CSharpFundamentals.Math;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CSharpFundamentals 
+{ 
+    
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var path = @"D:\ps\csharp\source\Servers\Servers.txt";
+            
+            // String processing for accessing path from strings
+            var index = path.IndexOf('.');
+            var fileext = path.Substring(index);
+            Console.WriteLine("File extension " + fileext);
+            Console.WriteLine();
+            // using path class
+            Console.WriteLine("Using Path Class");
+            Console.WriteLine();
+            Console.WriteLine("FileName: " + Path.GetFileName(path));
+            Console.WriteLine("FileName ext : " + Path.GetExtension(path));
+            Console.WriteLine("FileName without extensin : " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("DirectoryName :  " + Path.GetDirectoryName(path));
+
+
+
+
+
+        }
+
+
+
+    }
+}
