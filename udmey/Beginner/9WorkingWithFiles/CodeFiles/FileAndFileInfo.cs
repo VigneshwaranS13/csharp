92b7717b7023bf6d558af4a3253877c3b7b212a9
Vigneshwaran S <vigneshwarans013@gmail.com>
2020-12-24 21:23:46 +0530

    Added file fileinfo demo file

---
+++ udmey/Beginner/9WorkingWithFiles/CodeFiles/FileAndFileInfo.cs
@@ -0,0 +1,58 @@
+﻿using CSharpFundamentals.Math;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CSharpFundamentals 
+{ 
+    
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var path = @"D:\ps\csharp\source\Servers.txt";
+            var destination = @"D:\ps\csharp\destination\Servers.txt";
+            
+            //File calss
+
+            if(File.Exists(destination))
+            {
+                File.Delete(destination);
+            }                        
+            File.Copy(path, destination);//It will not overwrite the exisintg file            
+            File.Copy(path, destination, true);// It will overwrite the if the file is already exist
+            var content = File.ReadAllText(destination); ;
+            Console.WriteLine(content);//it will print the contents in the file
+
+            //FileInfo class
+
+            var fileInfo = new FileInfo(path);
+
+            
+            //fileInfo.CopyTo(destination);
+            fileInfo.CopyTo(destination, true);
+
+            if(fileInfo.Exists)
+            {
+                fileInfo.Delete();//This will permanently delete the source file 
+            }
+
+
+
+
+
+            
+
+
+
+        }
+
+
+
+    }
+}
