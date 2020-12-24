ed91339684b23f86604e885f50002804659853b3
Vigneshwaran S <vigneshwarans013@gmail.com>
2020-12-24 21:51:16 +0530

    Created dir info demo file

---
+++ udmey/Beginner/9WorkingWithFiles/CodeFiles/DirAndDirInfo.cs
@@ -0,0 +1,54 @@
+﻿using CSharpFundamentals.Math;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CSharpFundamentals 
+{ 
+    
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var path = @"D:\ps\csharp\source\Servers";
+            var destination = @"D:\ps\csharp\destination\Servers";
+
+            //Directory
+
+            Directory.CreateDirectory(path);//It will automatically overwrite  the folder if it exists
+            Directory.CreateDirectory(destination);
+
+            var directories = Directory.GetDirectories(@"D:\ps\", "*.*", SearchOption.AllDirectories);
+
+            foreach (var directory in directories)
+            {
+                Console.WriteLine(directory);
+            }
+
+            //DirectoryInfo
+            Console.WriteLine("****************************************DirectoryInfo**************************************");
+            var dirinfo = new DirectoryInfo(@"D:\ps\");
+            var dirs = dirinfo.GetDirectories();// GetDirectories method will return Array,so we need to assign and iterate to access individual elements
+
+            foreach(var dir in dirs)
+            {
+                Console.WriteLine(dir);
+            }
+
+
+
+
+
+
+
+        }
+
+
+
+    }
+}
