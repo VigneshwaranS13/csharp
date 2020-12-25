2769b7b1fe750b9b4c277464754bea879872244a
Vigneshwaran S <vigneshwarans013@gmail.com>
2020-12-25 23:24:44 +0530

    added edge effect code file

--- udmey/Beginner/10DebuggingApplications/Debugging.md
+++ udmey/Beginner/10DebuggingApplications/Debugging.md
@@ -6,3 +6,15 @@
 
 The execution will stop at the breakpoint
 
+## Edge cases/uncommon scnarios
+
+- defensive programming
+
+     when we get any input before processing that input we want to make sure
+it will satisfy certain criteria,else we are not gonna continut processing
+
+- Side effect
+
+Instead of directly processing the input we can make copy of the input and
+we can start processing
+	 
---
+++ udmey/Beginner/10DebuggingApplications/code/Edge_Effect.cs
@@ -0,0 +1,61 @@
+﻿using CSharpFundamentals.Math;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CSharpFundamentals 
+{ 
+    
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var numbers = new List<int> { 7, 8,9,15,5,3};
+            var smallests = GetSmallests(numbers, 3);
+
+            foreach(var number in smallests)
+                Console.WriteLine(number);
+
+            Console.WriteLine("Oriningal list");
+            //Edge code check
+            foreach (var number in numbers)
+                Console.WriteLine(number);
+            // so it will remove the inputs 
+
+        }
+
+        public static List<int> GetSmallests(List<int> list ,int count )
+        {
+            //to avoid edge effect
+            var buffer = new List<int>(list);//for avoiding edge effect 
+            var smallests = new List<int>();
+            smallests = new List<int>();
+
+            while (smallests.Count < count)
+            {
+                var min = GetSmallest(buffer);//we chaged from list to buffer
+                smallests.Add(min);
+                buffer.Remove(min);//we chaged from list to buffer
+            }
+            return smallests;
+        }
+
+        public static int GetSmallest(List<int> list)
+        {
+            // Assume the first number is the smallest
+            var min = list[0];
+            for(var i = 1;i<list.Count;i++)
+            {
+                if (list[i] < min)
+                    min = list[i];
+            }
+            return min;
+        }
+
+    }
+}
