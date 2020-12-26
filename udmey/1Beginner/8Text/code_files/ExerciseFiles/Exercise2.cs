422468c92fcf93ac8cdc4341ce90a632869b9566
Vigneshwaran S <vigneshwarans013@gmail.com>
2020-12-26 18:32:40 +0530

    Exercise2 version 1

---
+++ udmey/1Beginner/8Text/code_files/ExerciseFiles/Exercise2.cs
@@ -0,0 +1,54 @@
+﻿using CSharpFundamentals.Math;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace CSharpFundamentals
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            /** Write a program and ask the user to enter a few numbers separated by a hyphen. If the user simply presses
+            Enter, without supplying an input, exit immediately; otherwise, check to see if there are duplicates. If so,
+            display "Duplicate" on the console. **/
+
+            Console.WriteLine("Enter few numbers with hypen : ");
+            var Input = Console.ReadLine();
+
+            if (String.IsNullOrWhiteSpace(Input))
+            {
+                Console.WriteLine("No values has been entered");
+                
+            }
+            else
+            {
+                var buffer = Input.Replace('-', ' ');
+                var Numbers = buffer.Split(' ');
+                if(Numbers.Distinct().Count() ==Numbers.Count() )
+                {
+                    Console.WriteLine("No duplicates");
+                }
+                else
+                {
+                    Console.WriteLine("It has duplicates");
+                }
+           
+            }
+
+
+
+
+
+        }
+
+
+
+    }
+}
