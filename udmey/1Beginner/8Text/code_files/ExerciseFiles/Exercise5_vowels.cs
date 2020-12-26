c725805c37f34a480d4a5e7115bb189a73c09bde
Vigneshwaran S <vigneshwarans013@gmail.com>
2020-12-26 23:01:37 +0530

    Added 5th execercise file and renamed the files

---
+++ udmey/1Beginner/8Text/code_files/ExerciseFiles/Exercise5_vowels.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+namespace CSharpFundamentals
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //5 Write a program and ask the user to enter an English word. Count the number of vowels(a, e, o, u, i)
+            //in the word. So, if the user enters "inadequate", the program should display 6 on the console.
+
+            Console.WriteLine("Enter any word : ");
+            var input = Console.ReadLine();
+            int count = 0;
+
+            for(var i =0;i <input.Length;i++)
+            {
+                if(input[i]=='a' || input[i] == 'e'|| input[i] == 'i'|| input[i] == 'o'|| input[i] == 'u')
+                {
+                    count++;
+                }
+
+            }
+            Console.WriteLine("total number of vowels is : " + count);
+
+        }
+
+
+
+    }
+}
